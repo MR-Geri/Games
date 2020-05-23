46c2f96c734c383a314cd21d53aae083e5c99479
MR-Geri <63905039+MR-Geri@users.noreply.github.com>
2020-05-24 01:59:09 +0300

    работа над menu

--- Code/Start.py
+++ Code/Start.py
@@ -53,7 +53,6 @@ class Button:
         if x < mouse[0] < x + self.w and y < mouse[1] < y + self.h:
             pygame.draw.rect(display, self.active_color, (x, y, self.w, self.h))
             if click[0] == 1 and action is not None:
-                pygame.time.delay(300)
                 if action is quit:
                     pygame.quit()
                     quit()
@@ -71,7 +70,6 @@ class Button:
             pygame.draw.rect(display, self.inactive_color, (680, 700, 980, 600))
             print_text(action_info(), 700, 720)
             if click[0] == 1 and action is not None:
-                pygame.time.delay(50)
                 if action is quit:
                     pygame.quit()
                     quit()
@@ -83,23 +81,6 @@ class Button:
             pygame.draw.rect(display, self.inactive_color, (x, y, self.w, self.h))
             pygame.time.delay(100)
         print_text(message=message, x=x + self.ots_x, y=y + self.ots_y, font_size=size)
-
-
-def new_game():
-    display.blit(pygame.image.frombuffer(blur(), (1920, 1080), "RGB"), (0, 0))
-    preset_button = Button(w=480, h=50, x=75, y=14)
-    back_button = Button(w=110, h=50, y=14)
-    while True:
-        is_active_display()
-        preset_button.draw_info(100, 700, 'Бывалый выживальщик.', Presets().one, Presets().one_print)
-        back_button.draw(10, 10, 'Назад', menu)
-        for i in pygame.event.get():
-            if i.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if i.type == pygame.KEYDOWN and i.key == pygame.K_ESCAPE:
-                menu()
-        pygame.display.update()
 
 
 def save_game():
@@ -126,84 +107,124 @@ def save_game():
         print('Игра не создана.')
 
 
-def load_game():
-    def helper_load(number):
-        global person
-        if person is None:
-            temp = json.load(open('../Save_Loading/save.json'))
-            temp = temp[number]
-            person = Data_pers(len(temp), True)
-            for pers in range(len(temp)):
-                person.personalities[pers].name, person.personalities[pers].surname = temp[pers][0], temp[pers][1]
-                person.personalities[pers].age = temp[pers][2]
-                person.personalities[pers].special = set(temp[pers][3])
-                person.personalities[pers].skills = set(temp[pers][4])
-                person.personalities[pers].buff = set(temp[pers][5])
-                person.personalities[pers].de_buff = set(temp[pers][6])
-                person.personalities[pers].hp = temp[pers][7]
-                person.personalities[pers].hunger, person.personalities[pers].water = temp[pers][8], temp[pers][9]
-                person.personalities[pers].control, person.personalities[pers].stress = temp[pers][10], temp[pers][11]
-                person.personalities[pers].left_arm = temp[pers][12]
-                person.personalities[pers].right_arm = temp[pers][13]
-                person.personalities[pers].back, person.personalities[pers].pockets = temp[pers][14], temp[pers][15]
-            print('Игра загружена.')
-
-    display.blit(pygame.image.frombuffer(blur(), (1920, 1080), "RGB"), (0, 0))
-    back_button = Button(w=110, h=50, y=14)
-    saves_button = Button(w=480, h=50, x=160, y=10)
-    while True:
-        is_active_display()
-        pygame.draw.rect(display, (255, 255, 0), (700, 385, 520, 308))
-        back_button.draw(10, 10, 'Назад', menu)
-        saves_button.draw(720, 395, 'Ячейка 1.', helper_load(0))
-        saves_button.draw(720, 455, 'Ячейка 2.', helper_load(1))
-        saves_button.draw(720, 515, 'Ячейка 3.', helper_load(2))
-        saves_button.draw(720, 575, 'Ячейка 4.', helper_load(3))
-        saves_button.draw(720, 635, 'Ячейка 5.', helper_load(4))
-        for i in pygame.event.get():
-            if i.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if i.type == pygame.KEYDOWN and i.key == pygame.K_ESCAPE:
-                menu()
-        pygame.display.update()
-
-
-def options():
-    def volume_minus():
-        global volume
-        volume -= 0.01
-        volume = 0 if volume < 0 else volume
-        with open('../Save_Loading/settings.json', 'w') as file:
-            json.dump(volume, file, indent=2, ensure_ascii=False)
-        pygame.mixer_music.set_volume(volume)
-
-    def volume_plus():
-        global volume
-        volume += 0.01
-        with open('../Save_Loading/settings.json', 'w') as file:
-            json.dump(volume, file, indent=2, ensure_ascii=False)
-        pygame.mixer_music.set_volume(volume)
-
-    display.blit(pygame.image.frombuffer(blur(), (1920, 1080), "RGB"), (0, 0))
-    back_button = Button(w=110, h=50, y=14)
-    settings_opt = Button(w=100, h=100, x=35, y=35)
-    while True:
-        is_active_display()
-        pygame.draw.rect(display, (255, 255, 0), (700, 385, 520, 308))
-        back_button.draw(10, 10, 'Назад', menu)
-        settings_opt.draw(980, 400, '', volume_plus)
-        settings_opt.draw(1100, 400, '', volume_minus)
-        for i in pygame.event.get():
-            if i.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if i.type == pygame.KEYDOWN and i.key == pygame.K_ESCAPE:
-                menu()
-        pygame.display.update()
-
-
 def menu():
+    def new_game():
+        display.blit(pygame.image.frombuffer(blur(), (1920, 1080), "RGB"), (0, 0))
+        preset_button = Button(w=480, h=50, x=75, y=14)
+        back_button = Button(w=110, h=50, y=14)
+        while True:
+            is_active_display()
+            preset_button.draw_info(100, 700, 'Бывалый выживальщик.', Presets().one, Presets().one_print)
+            back_button.draw(10, 10, 'Назад', menu)
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if i.type == pygame.KEYDOWN and i.key == pygame.K_ESCAPE:
+                    menu()
+            pygame.display.update()
+
+    def load_game():
+        def helper_load(number):
+            global person
+            if person is None:
+                temp = json.load(open('../Save_Loading/save.json'))
+                temp = temp[number]
+                person = Data_pers(len(temp), True)
+                for pers in range(len(temp)):
+                    person.personalities[pers].name, person.personalities[pers].surname = temp[pers][0], temp[pers][1]
+                    person.personalities[pers].age = temp[pers][2]
+                    person.personalities[pers].special = set(temp[pers][3])
+                    person.personalities[pers].skills = set(temp[pers][4])
+                    person.personalities[pers].buff = set(temp[pers][5])
+                    person.personalities[pers].de_buff = set(temp[pers][6])
+                    person.personalities[pers].hp = temp[pers][7]
+                    person.personalities[pers].hunger, person.personalities[pers].water = temp[pers][8], temp[pers][9]
+                    person.personalities[pers].control, person.personalities[pers].stress = temp[pers][10], temp[pers][
+                        11]
+                    person.personalities[pers].left_arm = temp[pers][12]
+                    person.personalities[pers].right_arm = temp[pers][13]
+                    person.personalities[pers].back, person.personalities[pers].pockets = temp[pers][14], temp[pers][15]
+                print(person)
+                print(f'Игра загружена.')
+
+        class Helper_load:
+            def __init__(self, n):
+                self.n = n
+
+            def load(self):
+                helper_load(self.n)
+
+        display.blit(pygame.image.frombuffer(blur(), (1920, 1080), "RGB"), (0, 0))
+        back_button = Button(w=110, h=50, y=14)
+        saves_button = Button(w=480, h=50, x=160, y=10)
+        while True:
+            is_active_display()
+            pygame.draw.rect(display, (255, 255, 0), (700, 385, 520, 308))
+            back_button.draw(10, 10, 'Назад', menu)
+            saves_button.draw(720, 395, 'Ячейка 1.', Helper_load(0).load)
+            saves_button.draw(720, 455, 'Ячейка 2.', Helper_load(1).load)
+            saves_button.draw(720, 515, 'Ячейка 3.', Helper_load(2).load)
+            saves_button.draw(720, 575, 'Ячейка 4.', Helper_load(3).load)
+            saves_button.draw(720, 635, 'Ячейка 5.', Helper_load(4).load)
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if i.type == pygame.KEYDOWN and i.key == pygame.K_ESCAPE:
+                    menu()
+            pygame.display.update()
+
+    def options_game():
+        def volume_minus():
+            global volume
+            volume -= 0.001
+            volume = 0 if volume < 0 else volume
+            with open('../Save_Loading/settings.json', 'w') as file:
+                json.dump(volume, file, indent=2, ensure_ascii=False)
+            pygame.mixer_music.set_volume(volume)
+
+        def volume_plus():
+            global volume
+            volume += 0.001
+            with open('../Save_Loading/settings.json', 'w') as file:
+                json.dump(volume, file, indent=2, ensure_ascii=False)
+            pygame.mixer_music.set_volume(volume)
+
+        display.blit(pygame.image.frombuffer(blur(), (1920, 1080), "RGB"), (0, 0))
+        back_button = Button(w=110, h=50, y=14)
+        volume_button = Button(w=100, h=100, x=35, y=35)
+        while True:
+            is_active_display()
+            pygame.draw.rect(display, (255, 255, 0), (700, 385, 520, 308))
+            back_button.draw(10, 10, 'Назад', menu)
+            volume_button.draw(940, 420, '', volume_minus)
+            volume_button.draw(1080, 420, '', volume_plus)
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if i.type == pygame.KEYDOWN and i.key == pygame.K_ESCAPE:
+                    menu()
+            pygame.display.update()
+
+    def exit_game():
+        display.blit(pygame.image.frombuffer(blur(), (1920, 1080), "RGB"), (0, 0))
+        choice_button_no = Button(w=120, h=80, x=15, y=16)
+        choice_button_yes = Button(w=120, h=80, x=25, y=15)
+        while True:
+            is_active_display()
+            pygame.draw.rect(display, (255, 255, 0), (790, 510, 340, 100))
+            choice_button_yes.draw(800, 520, 'Да', quit, 50)
+            choice_button_no.draw(1000, 520, 'Нет', menu, 50)
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if i.type == pygame.KEYDOWN and i.key == pygame.K_ESCAPE:
+                    menu()
+            pygame.display.update()
+
     display.blit(back_menu, (0, 0))
     ex_button = Button(w=200, h=80, x=27, y=20)
     new_game_button = Button(w=280, h=50, x=52, y=13)
@@ -213,8 +234,8 @@ def menu():
         is_active_display()
         new_game_button.draw(820, 600, 'Новая игра', new_game)
         save_load_button.draw(820, 700, 'Загрузить игру', load_game)
-        options_button.draw(820, 800, 'Настройки', options)
-        ex_button.draw(860, 900, 'Выход', quit, 50)
+        options_button.draw(820, 800, 'Настройки', options_game)
+        ex_button.draw(860, 900, 'Выход', exit_game, 50)
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 pygame.quit()
